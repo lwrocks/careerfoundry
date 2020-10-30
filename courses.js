7719af9998d384c0e24385b485f9e56182eb16f4
Laurel Wright <wright.laureld@gmail.com>
2020-10-30 12:37:53 +0100

    Figured out HTML rendering

--- courses.js
+++ courses.js
@@ -1,15 +1,31 @@
-let course_list = [];
-const container = document.getElementById('course-container');
-const url = 'https://private-e05942-courses22.apiary-mock.com/courses';
+// Display CareerFoundry Courses
+const url = "https://private-e05942-courses22.apiary-mock.com/courses";
 
-fetch(url)
-    .then((response) => response.json())
-    .then(data => {
-        course_list = data.message;
-        for (title in course_list) {
-            let li = document.createElement('li');
-            let node = document.createTextNode(title);
-            li.appendChild(node);
-            container.appendChild(li);
-        }
+function fetchData() {
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error("ERROR");
+      }
+      return response.json();
     })
+    .then((data) => {
+      console.log(data);
+      const html = data
+        .map((course) => {
+          return `<div class="course-info">
+              <p>Title: ${course.title}</p>
+              <p>Author: ${course.author}</p>
+              <button><a href="${course.url}">Learn More</a></button>
+            </div>`;
+        })
+        .join("");
+      console.log(html);
+      document.querySelector("#app").insertAdjacentHTML("afterbegin", html);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+fetchData();
